b7d2bc6edf856f0857c39ab2654a5e2af0e3405b
admin <admin@DESKTOP-8VQH0LB>
2024-10-14 11:06:46 -0700

    Add project files.

---
+++ A1task3/A1task3.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a=7,b=9;
+	cout << " The sum of the number is:";
+	int sum = a+b;
+	cout << sum <<endl;
+	int c=3,d=8;
+	cout << " The difference of the number is:";
+	int sub = c-d;
+	cout << sub << endl;
+	int e=5,f=4;
+	cout << " The product of the number is:";
+	int multi = e * f;
+	cout << multi << endl;
+	float g=9,h=2;
+	cout << " The division of the number is:";
+	float divide = g / h;
+	cout << divide << endl;
+
+	return 0;
+}
+
